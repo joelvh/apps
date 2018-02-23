1cf8ef17a39d3e8c781e97c8b9055483cf98cd9e
Joel Van Horn <joel@joelvanhorn.com>
2018-02-23 00:22:10 -0500

    Add option to not use "pretty" output in script

--- lib/apps/adapters/gmail/markup/base_context.rb
+++ lib/apps/adapters/gmail/markup/base_context.rb
@@ -9,8 +9,8 @@ module Apps
             { "@context" => "http://schema.org" }.merge(super)
           end
 
-          def to_script
-            %`<script type="application/ld+json">\n#{to_json(pretty: true)}\n</script>`
+          def to_script(pretty: true)
+            %`<script type="application/ld+json">\n#{to_json(pretty: pretty)}\n</script>`
           end
         end
       end
